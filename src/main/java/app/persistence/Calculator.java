cac6db9dcf83adc359784fb35b5623bb7f434d6a
MGold <mikkelgold@protonmail.com>
2023-12-06 16:23:29 +0100

    Tror jeg blev færdig med en Calculator

--- src/main/java/app/persistence/Calculator.java
+++ src/main/java/app/persistence/Calculator.java
@@ -2,12 +2,7 @@ package app.persistence;
 
 import app.exceptions.DatabaseException;
 import app.models.Orders;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Calculator {
 
