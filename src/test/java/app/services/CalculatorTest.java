de836300dde15aa2bdebaceb7726d8646e2e8b34
MGold <mikkelgold@protonmail.com>
2023-12-07 13:01:32 +0100

    Prøvede at kalde vores calculator og længde og bredde til næste html-side og uploade i database og lavet en test

---
+++ src/test/java/app/services/CalculatorTest.java
@@ -0,0 +1,26 @@
+package app.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    int carport_length= 500;
+    int carport_width = 350;
+    @Test
+    void calculatePost() {
+    }
+
+    @Test
+    void calculateRafter() {
+    }
+
+    @Test
+    void calculateStraps() {
+    }
+
+    @Test
+    void calculateStrapsWidth() {
+    }
+}
